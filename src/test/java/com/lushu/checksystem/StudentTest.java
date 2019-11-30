9e8a0071fa53816c683df8408f607603b94992b0
ALuShu <1003454023@qq.com>
2019-11-30 17:21:53 +0800

    nothing to do,learn the simhash

--- src/test/java/com/lushu/checksystem/StudentTest.java
+++ src/test/java/com/lushu/checksystem/StudentTest.java
@@ -6,6 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.lushu.checksystem.service.StudentService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.xzixi.algorithm.simhash.common.util.CommonUtils;
+import com.xzixi.algorithm.simhash.common.AbstractKeywordsExtractor;
+import com.xzixi.algorithm.simhash.common.Config;
+import com.xzixi.algorithm.simhash.common.HashGenerator;
+import com.xzixi.algorithm.simhash.common.KeywordsExtractor;
+import com.xzixi.algorithm.simhash.common.SimHashException;
 
 /**
  * @author lushu
